be300895797657254e26216ecc17d64c17fdbe1f
Caleb Matthews <126044281+caleb1996bit@users.noreply.github.com>
2023-06-05 22:51:15 -0500

    add hover effect

--- javascript.js
+++ javascript.js
@@ -6,29 +6,26 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "grid-item";  
+
+
+     //👇️ Change text color on mouseover
+cell.addEventListener('mouseover', function handleMouseOver() {
+  cell.style.backgroundColor = 'green'
+});
+
+
+// 👇️ Change text color back on mouseout
+cell.addEventListener('mouseout', function handleMouseOut() {
+  cell.style.backgroundColor = 'yellow'
+} 
+
+); 
+
   };
 };
 
 
-/* 👇️ Change text color on mouseover
-container.addEventListener('mouseover', function handleMouseOver() {
-  container.style.color = 'red';
-});//
 
-/* 👇️ Change text color back on mouseout
-container.addEventListener('mouseout', function handleMouseOut() {
-  container.style.backgroundcolor = 'black';
-});*/ 
-
-container.addEventListener('mouseover', 
-  e => e.target.classList.add('grid-item')
-) 
-
-let currentColor = 'black'
-
-const colors = { black: 'black' }
-
-e => e.target.classList.add(colors[currentColor])
 
 console.log(makeRows(16, 16));
